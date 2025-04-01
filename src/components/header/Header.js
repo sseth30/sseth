fc733c6cbb39a4d53398efbe9dec5ec5d85548bf
Satchit Seth <satchit.seth30@gmail.com>
2025-04-01 17:13:41 -0400

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -28,7 +28,7 @@ function Header() {
   return (
     <Headroom>
       <header className={isDark ? "dark-menu header" : "header"}>
-        <a href="/sseth" className="logo">
+        <a href="/" className="logo">
           <span className="grey-color"> &lt;</span>
           <span className="logo-name">{greeting.username}</span>
           <span className="grey-color">/&gt;</span>
