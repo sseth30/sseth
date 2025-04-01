2ee5ffac800470361e9cf83371805fb27e1dc7de
Satchit Seth <satchit.seth30@gmail.com>
2025-04-01 16:29:33 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.scss";
 import Main from "./containers/Main";
 import AboutMe from "./containers/about/AboutMe";
