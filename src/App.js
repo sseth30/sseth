9c4ec11db7d767d3afc8923622a153294dfdbe4b
Satchit Seth <satchit.seth30@gmail.com>
2025-04-01 15:38:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
+import About from "./containers/about/About"; // You'll create this page
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Main />
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route path="/" component={Main} />
+      </Switch>
+    </Router>
   );
 }
 
