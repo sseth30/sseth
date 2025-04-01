aa6ce7747f656c8521fe0f8289c948b9782c2b87
Satchit Seth <satchit.seth30@gmail.com>
2025-04-01 15:40:04 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.scss";
 import Main from "./containers/Main";
-import About from "./containers/about/About"; 
+import About from "./containers/about/AboutMe"; 
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
