8009ac65a9d42179dcd2bc565b62792e068476f5
Satchit Seth <satchit.seth30@gmail.com>
2025-04-01 23:32:53 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
 import Main from "./containers/Main";
 import AboutMe from "./containers/about/AboutMe";
+import StyleContext from "./contexts/StyleContext";
 
 function App() {
+  const { isDark } = useContext(StyleContext);
+
   return (
-    <Router basename="/sseth">
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route path="/about" component={AboutMe} />
-      </Switch>
-    </Router>
+    <div className={isDark ? "App dark-mode" : "App"}>
+      <Router basename="/sseth">
+        <Switch>
+          <Route exact path="/" component={Main} />
+          <Route path="/about" component={AboutMe} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
