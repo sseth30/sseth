6b85619f1269d52d21e00938ec81337a06408850
Satchit Seth <satchit.seth30@gmail.com>
2025-04-16 01:22:02 -0400

    Update Greeting.js

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -28,8 +28,8 @@ export default function Greeting() {
           <Fade bottom duration={1000} distance="40px">
             <div>
               <h1 className={isDark ? "dark-mode greeting-text" : "greeting-text"}>
-                {greeting.title} <span className="wave-emoji">{emoji("👋")}</span>
-              </h1>
+                Hi, I'm Satchit <span className="wave-emoji">{emoji("👋")}</span>
+                  </h1>
               <p className={isDark ? "dark-mode greeting-text-p" : "greeting-text-p subTitle"}>
                 {greeting.subTitle}
               </p>
